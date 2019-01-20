60278360eb772e649414bfd700ee2aba37a2ed72
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-20 09:52:38 +0100

    Removed test.php file

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'TCPDF/tcpdf.php';
-require 'src/nen3140/pdfgen.php';
-
-define('INSPECTIES_ASSETS_DIR', dirname(__FILE__) . '/assets');
-
-Tlc\pdfgen(array('path' => dirname(__FILE__) . '/test.pdf'));
